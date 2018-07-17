ae80e2e06529bf13d67778e6b6a070b8b5dbe1f7
Vincent Aduuna Frimpong <tossia13@gmail.com>
2018-07-17 12:39:30 +0100

    edits

--- ejumaapp.py
+++ ejumaapp.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Job
 
 @app.route('/')
 def index():
@@ -8,5 +14,4 @@ def index():
 
 if __name__=='__main__':
     import os
-    app.run(host='0.0.0.0', debug=True, port=int(os.environ.get('PORT',8000)))
-    
+    app.run(host='0.0.0.0', debug=True, port=int(os.environ.get('PORT',8000))) 
