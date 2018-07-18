4ac69a8141bfbec2b0b34f338f50eee8778885ec
estyvalankomah <abenammra2002@gmail.com>
2018-07-17 17:31:14 -0700

    new file

---
+++ templates/routes.py
@@ -0,0 +1,15 @@
+@app.route("/home")
+def home():
+    return "Home page"
+
+@app.route("/signup")
+def signup():
+    return "Sign up page"
+	
+@app.route("/login")
+def login():
+    return "Login page"	
+	
+@app.route("/about")
+def signup():
+    return "About page"	
